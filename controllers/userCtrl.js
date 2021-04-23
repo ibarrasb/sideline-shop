9b00b1184d1b86dfc06ce39db33a222fa31f4d37
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-04-23 09:49:20 -0500

    jwt authentication working, token and refresh token implimented

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,8 +1,10 @@
 const Users = require('../models/userModel');
-const { use } = require('../routes/userRouter');
-const userCtrl = {
+// const { use } = require('../routes/userRouter');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-    register: async(req,res) => {
+const userCtrl = {
+ register: async(req,res) => {
         try{
             const{name, email, password} = req.body;
 
@@ -11,11 +13,66 @@ const userCtrl = {
 
             if(password.length < 6 )
             return res.status(400).json({msg: 'Password is at least 6 characters long'})
+            
+            // Password Encryption
+            const passwordHash = await bcrypt.hash(password, 10)
+            const newUser = new Users({
+                name, email, password: passwordHash
+            })
+
+            //Save User to MongoDB
+            await newUser.save()
+
+            //Then create jsonwebtoken to authentication
+            const accesstoken = createAccessToken({id: newUser._id})
+            const refreshtoken = createRefreshToken({id: newUser._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true, 
+                path: '/user/refresh_token'
+            })
+
+            res.json({accesstoken})
         
-        
-        }catch (err) {
+        } catch (err) {
             return res.status(500).json({msg: err.message})
         }
+    }, 
+    login: async (req, res) => {
+        try {
+            const {email, password} = req.body;
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+
+    },
+    refreshToken: (req, res) => {
+        try {
+        const rf_token = req.cookies.refreshtoken;
+        if(!rf_token) return res.status(400).json({msg: "Please Log in or Register"})
+        
+        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+            if(err) return res.status(400).json({msg: "Please Log in or Register"})
+            const accesstoken = createAccessToken({id: user.id})
+
+            res.json({accesstoken})
+        })
+
+    
+        } catch (err){
+            return res.status(500).json({msg: err.message})
+        }
+        
+
     }
+    
 }
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+}
+
 module.exports = userCtrl
