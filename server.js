ef48466e71293f3c55e5fc2d80eb9392f8202723
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-04-23 00:58:29 -0500

    update

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+//connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err =>{
+    if(err) throw err; 
+    console.log("Connected to MongoDB")
+})
+app.get('/', (req, res) => {
+    res.json({msg: "Welcome to sideline"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT)
+})
