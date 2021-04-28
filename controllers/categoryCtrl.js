a7823aeef8c6c6a4a5988ac02654b387b125dde6
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-04-27 20:02:13 -0500

    CRUD for all category, and admin access

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -1,9 +1,48 @@
 // const { models } = require("mongoose")
-
+const Category = require('../models/categoryModel')
 const categoryCtrl = {
-    getCategories: async(req, res) =>{
-        res.json('category test ctrl')
+getCategories: async(req, res) =>{
+        try {
+            const categories = await Category.find()
+            res.json(categories)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+            
+        }
+},
+createCategory: async(req, res) =>{
+    try {
+        const {name} = req.body;
+        const category = await Category.findOne({name})
+        if(category) return res.status(400).json({msg: "This category already exists"})
+
+        const newCategory = new Category({name})
+
+        await newCategory.save()
+        res.json({msg: "Created category successfully"})
+        
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
     }
+},
+deleteCategory: async(req, res) =>{
+    try {
+        await Category.findByIdAndDelete(req.params.id)
+        res.json({msg: "Deleted a category"})
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+},
+updateCategory: async(req, res) =>{
+    try {
+        const {name} = req.body;
+        await Category.findOneAndUpdate({_id: req.params.id}, {name})
+
+        res.json({msg: "Updated category successfully"})
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+}
 
 }
 module.exports = categoryCtrl
