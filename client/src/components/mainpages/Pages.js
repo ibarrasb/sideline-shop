3e8126144b369a0b026ca9e433691b74db8b579f
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-05-05 22:06:19 -0500

    more client side components added and routing

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import {Switch, Route } from 'react-router-dom'
+import Products from './products/Products'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import Cart from './cart/Cart'
+import NotFound from './utils/not_found/NotFound'
 function Pages() {
     return (
-        <div>
-            Main page Component
-        </div>
+        <Switch>
+            <Route path='/' exact component={Products}/>
+            <Route path='/login' exact component={Login}/>
+            <Route path='/register' exact component={Register}/>
+            <Route path='/cart' exact component={Cart}/>
+
+            <Route path='*' exact component={NotFound}/>
+        </Switch>
     );
 }
 
