2784d7eeb0912f35ac888b53c4931482fe9395b5
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-07 15:24:12 -0500

    set login and set admin, constant refresh tokens and logout

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,15 +1,27 @@
-import React, {createContext, useState} from 'react'
+import axios from 'axios'
+import React, {createContext, useEffect, useState} from 'react'
 import ProductsAPI from './api/ProductsAPI'
+import UserAPI from './api/UserAPI'
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) => {
     const [token, setToken] = useState(false)
 
-    
+    const refreshToken = async () =>{
+        const res = await axios.get('/user/refresh_token')
+        
+        setToken(res.data.accesstoken)
+    }    
+    useEffect(() =>{
+        const firstLogin = localStorage.getItem('firstLogin')
+       if(firstLogin) refreshToken()
+    }, [])
+
 const state = {
     token: [token, setToken],
-    ProductsAPI: ProductsAPI()
+    ProductsAPI: ProductsAPI(),
+    UserAPI: UserAPI(token)
 }
     return (
         <GlobalState.Provider value={state}>
--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -4,9 +4,37 @@ import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
 import {Link} from 'react-router-dom'
+import axios from 'axios';
 
 function Header() {
-    const value = useContext(GlobalState)
+    const state = useContext(GlobalState)
+    const [isLogged, setIsLogged] = state.UserAPI.isLogged
+    const [isAdmin, setIsAdmin] = state.UserAPI.isAdmin
+
+    const logoutUser = async () => {
+        await axios.get('/user/logout')
+        localStorage.clear()
+        setIsAdmin(false)
+        setIsLogged(false)
+    }
+
+    const adminRouter = () => {
+        return(
+            <div>
+            <li><Link to="/create_product">Create Product</Link></li>
+            <li><Link to="/category">Categories</Link></li>
+            </div>
+        )
+    }
+    const loggedRouter = () => {
+        return(
+            <div>
+            <li><Link to="/history">History</Link></li>
+            <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
+            </div>
+        )
+    }
+
     return (
     <header>
         <div className='menu'>
@@ -15,18 +43,27 @@ function Header() {
 
         <div className='logo'>
             <h1>
-                <Link to="/">Sideline</Link>
+                    <Link to="/">{isAdmin ? 'Admin' : 'Sideline'}</Link>
             </h1>
         </div>
 
         <ul>
-            <li><Link to="/">Products</Link></li>
-            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/">{isAdmin ? 'Admin' : 'Shop'}</Link></li>
+
+            {isAdmin && adminRouter()}
+            
+            {
+                isLogged ? loggedRouter() :  <li><Link to="/login">Login</Link></li>
+            }
+           
             <li>
-                <img src={Close} alt ="" width="30"/>
+                <img src={Close} alt ="" width="30" className="menu"/>
             </li>
+
         </ul>
-        <div className="cart-icon">
+        {
+            isAdmin ? '' :
+             <div className="cart-icon">
 
             <span>0</span>
             <Link to="/cart">
@@ -35,6 +72,8 @@ function Header() {
 
         </div>
         
+        }
+       
     </header>
     );
 }
