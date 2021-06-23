9c2d249ff7c2130fc806eb86bd97ee8471f3633c
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-23 00:01:37 -0500

    netlify

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0',() => {
+app.listen(PORT,() => {
     console.log('Server is running on port', PORT)
 })
