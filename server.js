bfd32e3f06e769ebd057e11e7169c5b8513f9bfc
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-23 00:57:38 -0500

    new

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT,() => {
+
+app.listen(process.env.PORT || 5000,() => {
     console.log('Server is running on port', PORT)
 })
