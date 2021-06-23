7732c2edaf5e25060a80917b5b5be92dde7b3925
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-22 22:10:06 -0500

    host

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
     res.json({msg: "Welcome to sideline"})
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
