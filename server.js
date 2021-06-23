8740ef2c9c7b92b57fcf9b1474e940f96742c064
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-06-22 21:28:00 -0500

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const path= require("path");
 
 const app = express()
 app.use(express.json())
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
     });
 }
+
 
 //connect to mongodb
 const URI = process.env.MONGODB_URL
